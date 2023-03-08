b592ed877375034da6b3aa038554dbcec9b54e3d
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-03-08 00:14:59 -0300

    fix formatting

--- gol_random.c
+++ gol_random.c
@@ -121,7 +121,7 @@ void copia_matriz(int a[ROWS][COLS], int b[ROWS][COLS]) {
 
 void restaura_cursor() {
     // reposicionar cursor
-    printf("\033[%dB", ROWS/2 + 1);
+    printf("\033[%dB\n", ROWS/2);
     // mostrar cursor
     printf("\033[?25h");
 }
